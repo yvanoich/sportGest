5cdff010878e2efe1523fea317953d4e0a53228e
yvanoich <yvann.om@orange.fr>
2024-02-06 20:26:04 +0100

    add profile image on activity home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,20 +9,25 @@
 <div class="container home">
     @foreach ($activitys as $activity)
         <div class="bloc-activity">
-            <div class="activity-util">{{ $activity->user->name }}</div>
-            <div class="activity-date">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $activity->date)->locale('fr_FR')->format('d M Y à H:i') }}</div>
-            <div class="activity-name"><a href="{{ route('activity-get', ['ident' => $activity->ident]) }}">{{ $activity->name }}</a></div>
-            @if($activity->description)
-                <div class="activity-description">{{ $activity->description }}</div>
-            @endif
-            <div class="informations">
-            <?php
-                $duration = \Carbon\CarbonInterval::seconds($activity->duration)->cascade();
-                $formattedDuration = $duration->hours > 0 ? $duration->format('%H:%I:%S') : $duration->format('%I:%S');
-            ?>
-                <div class="activity-distance">{{ $activity->distance }} km</div>
-                <div class="activity-average-speed">{{ \Carbon\CarbonInterval::seconds($activity->duration / $activity->distance)->cascade()->format('%I:%S') }} / km</div>
-                <div class="activity-duration">{{ $formattedDuration }}</div>
+            <div class="activity-logo">
+                <img src="{{ file_exists(public_path('images/profiles/' . $activity->user->ident . '.jpg')) ? asset('images/profiles/' . $activity->user->ident . '.jpg') : asset('images/profiles/icone_user.jpg') }}" alt="Image de profil">
+            </div>
+            <div class="activity-right">
+                <div class="activity-util">{{ $activity->user->name }}</div>
+                <div class="activity-date">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $activity->date)->locale('fr_FR')->format('d M Y à H:i') }}</div>
+                <div class="activity-name"><a href="{{ route('activity-get', ['ident' => $activity->ident]) }}">{{ $activity->name }}</a></div>
+                @if($activity->description)
+                    <div class="activity-description">{{ $activity->description }}</div>
+                @endif
+                <div class="informations">
+                <?php
+                    $duration = \Carbon\CarbonInterval::seconds($activity->duration)->cascade();
+                    $formattedDuration = $duration->hours > 0 ? $duration->format('%H:%I:%S') : $duration->format('%I:%S');
+                ?>
+                    <div class="activity-distance">{{ $activity->distance }} km</div>
+                    <div class="activity-average-speed">{{ \Carbon\CarbonInterval::seconds($activity->duration / $activity->distance)->cascade()->format('%I:%S') }} / km</div>
+                    <div class="activity-duration">{{ $formattedDuration }}</div>
+                </div>
             </div>
         </div>
     @endforeach
